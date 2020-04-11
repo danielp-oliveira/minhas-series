c02f2f9c5da7a3a4afc9be28a475d7e7e569fc91
Daniel Oliveira <danielp.oliveira@outlook.com>
2020-04-10 22:48:21 -0300

    starting navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react"
 
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+} from "reactstrap"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Minhas Séries</h1>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>Minhas Séries</NavbarBrand>
+        <NavbarToggler />
+        <Collapse isOpen={true} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Genêros</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import * as serviceWorker from "./serviceWorker"
 
 import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
 
 serviceWorker.unregister()
