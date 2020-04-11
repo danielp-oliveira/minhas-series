747f2f9aff0ef15df5091156cd2435b9c8bc520b
Daniel Oliveira <danielp.oliveira@outlook.com>
2020-04-11 11:22:03 -0300

    Adding axios and testing api

--- src/pages/Genres/index.js
+++ src/pages/Genres/index.js
@@ -1,5 +1,46 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-const GenresPage = () => <h1>Genres Page</h1>
+import { Alert, Container, Table } from "reactstrap"
+
+const GenresPage = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/genres").then(({ data: { data } }) => {
+      setData(data)
+    })
+  }, [])
+
+  return (
+    <Container>
+      <h1>Genres</h1>
+      {data.length === 0 ? (
+        <Alert color="warning">Você não possui genêros criados.</Alert>
+      ) : (
+        <Table dark>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((genre, i) => {
+              return (
+                <tr key={i}>
+                  <th scope="row">{genre.id}</th>
+                  <th>{genre.name}</th>
+                  <th></th>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      )}
+    </Container>
+  )
+}
 
 export default GenresPage
